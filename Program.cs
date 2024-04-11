c32870c7d77a8854f073e89fc13f755b10131255
Xabibullayev Kamronbek <simplekux8082@icloud.com>
2024-04-11 19:12:56 +0500

    did

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿Console.WriteLine("Hi user !");
+Console.WriteLine("Choose menu :");
+Console.WriteLine("1. +, -, *, /, ^ does solve");
+Console.WriteLine("2. log a (b)");
+
+string input = Console.ReadLine();
+switch(input)
+{
+    case "1" :
+        {
+            Console.Write("ENTER the expressions : ");
+            string ifoda = Console.ReadLine();
+            string amal = "";
+            if(ifoda.Contains("+"))
+            {
+                amal = "+";
+            }
+            else if(ifoda.Contains("-"))
+            {
+                amal = "-";
+            }
+            else if(ifoda.Contains("*"))
+            {
+                amal = "*";
+            }
+            else if(ifoda.Contains("/"))
+            {
+                amal = "/";
+            }
+            else if(ifoda.Contains("^"))
+            {
+                amal = "^";
+            }
+            string strNum1 = ifoda.Substring(0, ifoda.IndexOf(amal));
+            string strNum2 = ifoda.Substring(ifoda.IndexOf(amal) + 1);
+            double firstNumber = Convert.ToDouble(strNum1);
+            double secondNumber = Convert.ToDouble(strNum2);
+            double natija = amal switch
+            {
+                "+" => firstNumber + secondNumber,
+                "-" => firstNumber - secondNumber,
+                "*" => firstNumber * secondNumber,
+                "/" => firstNumber / secondNumber,
+                "^" => Math.Pow(firstNumber, secondNumber),
+            }; 
+            Console.WriteLine($"{ifoda} = {natija}");
+        }
+        break;
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Easy-calculator
+ # Last result
+
+ ## Hi user !
+### Choose menu :
+### 1. +, -, *, /, ^ does solve
+### 2. log a (b)
+### 1
+### ENTER the expressions : 15 +  25
+### 15 +  25 = 40
